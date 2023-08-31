10f64a010c67272af57e07a49c27f79f1cf59595
Tiago Facco <thifacco@gmail.com>
2023-08-31 14:29:27 -0300

    chore: roteamento com página home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    loadChildren: () => import('./home/home.module').then((routes) => routes.HomeModule)
   },
   {
     path: 'books',
