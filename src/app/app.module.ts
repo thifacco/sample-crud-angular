1661db9def000266809ee04aacaa1026327f14db
Tiago Facco <thifacco@gmail.com>
2023-08-31 11:28:12 -0300

    chore: estrutura de crud simples para livros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './crud-simple/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
